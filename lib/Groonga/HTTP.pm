7cfddab4bfce0fbddf7b438a026004160d65f958
wata <nagasawa@junkapp.com>
2011-07-18 04:20:46 +0900

    initial commit

---
+++ lib/Groonga/HTTP.pm
@@ -0,0 +1,32 @@
+package Groonga::HTTP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Groonga::HTTP -
+
+=head1 SYNOPSIS
+
+  use Groonga::HTTP;
+
+=head1 DESCRIPTION
+
+Groonga::HTTP is
+
+=head1 AUTHOR
+
+Wataru Nagasawa E<lt>nagasawa {at} junkapp.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
